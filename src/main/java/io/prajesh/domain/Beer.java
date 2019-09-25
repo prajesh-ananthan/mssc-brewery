3eaed722e498bd45422e6330ce726c617fcad56e
Prajesh Ananthan <prajesh.ananthan@outlook.com>
2019-09-26 00:15:11 +0800

    Added Beer entity 57 - JPA Entities

---
+++ src/main/java/io/prajesh/domain/Beer.java
@@ -0,0 +1,47 @@
+package io.prajesh.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+/**
+ * @author Prajesh Ananthan
+ * Created on 26/9/2019.
+ */
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+  @Id
+  @GeneratedValue(generator = "UUID")
+  @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+  @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+  private UUID uuid;
+
+  @Version
+  private Long version;
+  private String beerName;
+  private String beerStyle;
+
+  @Column(unique = true)
+  private Long upc;
+  private BigDecimal price;
+
+  @CreationTimestamp
+  @Column(updatable = false)
+  private Timestamp createdDate;
+
+  @UpdateTimestamp
+  private Timestamp lastModifiedDate;
+}
